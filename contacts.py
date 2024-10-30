9554503efde4639d048a3555e6528d04d5e315b6
slarsen1706 <45298641+slarsen1706@users.noreply.github.com>
2024-10-30 10:26:48 +0100

    Documented Contacts
    Wrote documentation for the contacts file

--- contacts.py
+++ contacts.py
@@ -2,12 +2,16 @@ from jsonHandler import *
 
 class Contacts(object):
     def __init__(self, user):
+        #Get the messages stored in the JSON file
         messages = FetchMessageJSON()
         self.contacts = []
+        #Loop over all the messages, if they are from/to the user, then they are added to the contacts
         for sender in messages:
             for receiver in messages[sender]:
+                #If the person is already in the contacts, skip them
                 if (sender in self.contacts) or (receiver in self.contacts):
                     continue
+                #If either the sender or receiver is the user, add them in the contacts
                 if receiver == user or sender == user:
                     self.contacts.append(receiver)
                     
